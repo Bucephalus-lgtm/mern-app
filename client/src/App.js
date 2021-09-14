8066bd6ebfc58bb8883fef46d3fa41535fefec7b
Bucephalus <bhargabnath691@gmail.com>
2021-09-15 01:20:15 +0530

    slight ui change

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [response, setResponse] = useState('Oops, something went wrong...')
-    useEffect(() =>{
-        async function checkAPIRoute() {
-            try {
-                const host = process.env.REACT_APP_HOST || "http://localhost:5000"
-                const response = await fetch(
-                    `${host}/api`
-                )
-                if (response.status === 200) {
-                    setResponse("a Success!!!")
-                }
-            } catch (err) {
-                setResponse("Oops, something went wrong...")
-            }
+  const [response, setResponse] = useState('Oops, something went wrong...')
+  useEffect(() => {
+    async function checkAPIRoute() {
+      try {
+        const host = process.env.REACT_APP_HOST || "http://localhost:5000"
+        const response = await fetch(
+          `${host}/api`
+        )
+        if (response.status === 200) {
+          setResponse("a Success!!!")
         }
-        checkAPIRoute()
-    }, [response])
+      } catch (err) {
+        setResponse("Oops, something went wrong...")
+      }
+    }
+    checkAPIRoute()
+  }, [response])
 
   return (
-    <div style={{background: '#0f4b6d', color: '#fff'}}>
-      <header className="App-header">
-        <h1>Bhargab Nath, Full Stack MERN Developer</h1>
+    <div className='container' style={{ background: '#0f4b6d', color: '#fff' }}>
 
+      <div className="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Secondary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <header className="App-header">
         <h1>The API call is...</h1>
-          <h2>{response}</h2>
+        <h2>{response}</h2>
       </header>
     </div>
   );
